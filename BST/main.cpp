06ca2a7ccefdcec27273eff89e44169f4bda2f1a
AlanHom <66947896@qq.com>
2021-12-14 09:19:41 +0800

    二分搜索树的基础框架以及节点插入操作

---
+++ BST/main.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+
+using namespace std;
+
+// 二分搜索树
+template<typename Key, typename Value>
+class BST {
+
+private:
+	// 二分搜索树中的节点为私有结构体，外界不需要了解二分搜索树节点的具体实现
+	struct Node {
+		Key key;
+		Value value;
+		Node* left;
+		Node* right;
+
+		Node(Key key, Value value) {
+			this->key = key;
+			this->value = value;
+			this->left = this->right = nullptr;
+		}
+	};
+
+	Node* root;		// 根节点
+	int count;		// 节点个数
+
+public:
+	BST() {		// 默认构造一棵空二分搜索树
+		root = nullptr;
+		count = 0;
+	}
+	~BST() {
+		// TODO:
+	}
+
+	// 返回二分搜索树的节点个数
+	int size() const {
+		return count;
+	}
+
+	// 返回二分搜索树是否为空
+	bool isEmpty() const {
+		return count == 0;
+	}
+
+	// 向二分搜索树中插入一个新的(key, value)数据对
+	void insert(Key key, Value value) {
+		root = insert(root, key, value);
+	}
+
+	bool contain(Key key) {
+		return contain(root, key);
+	}
+
+	Value* search(Key key) {
+		retturn search(root, key);
+	}
+
+private:
+	// 向以node为根的二叉搜索树中，插入节点(key, value)，使用递归算法
+	// 返回插入新节点后的二叉搜索树的根
+	Node* insert(Node* node, Key key, Value value) {
+		
+		if (node == nullptr) {
+			count++;
+			return new Node(key, value);
+		}
+		if (key == node->key)
+			node->value = value;
+		else if (key < node->key)
+			node->left = insert(node->left, key, value);
+		else // key > node->key
+			node->right = insert(node->right, key, value);
+		return node;
+	}
+
+	// 查看以node为根节点的二叉搜索树中是否包含键值为key的节点
+	bool contain(Node* node, Key key) {
+
+		if (node == nullptr)
+			return false;
+		if (key == node->key)
+			return true;
+		else if (key < node->key)
+			return contain(node->left, key);
+		else
+			return contain(node->right, key);
+	}
+
+	// 在以node为根的二叉搜索树中查找key所对应的value
+	Value* search(Node* node, Key key) {
+
+		if (node == nullptr)
+			return nullptr;
+		if (key == node->key)
+			return &(node->value);
+		else if (key < node->key)
+			return search(node->left, key);
+		else
+			return search(node->right, key);
+	}
+};
+
+int main() {
+
+	return 0;
+}
